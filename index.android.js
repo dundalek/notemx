187b4eb54daabbfddadb2a13a795f565d33a85bd
Jakub Dundalek <dundalek@gmail.com>
2017-02-18 18:07:28 +0100

    Checkpoint

--- index.android.js
+++ index.android.js
@@ -3,19 +3,32 @@
 import React, { Component } from 'react';
 import { Text, Navigator, TouchableHighlight, AppRegistry, ToolbarAndroid, StyleSheet, ListView, View, TextInput, BackAndroid } from 'react-native';
 import ActionButton from 'react-native-action-button';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 import Icon from 'react-native-vector-icons/Ionicons';
+import FontIcon from 'react-native-vector-icons/FontAwesome';
 
 var _navigator;
 
-BackAndroid.addEventListener('hardwareBackPress', () => {
-  if (_navigator.getCurrentRoutes().length === 1  ) {
-     return false;
-  }
-  _navigator.pop();
-  return true;
-});
-
 export default class NotesApp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [
+        { id: 1, title: 'hello', folder: true },
+        { id: 2, title: 'world', content: 'hello' }
+      ]
+    };
+
+    BackAndroid.addEventListener('hardwareBackPress', () => {
+      this.saveNote();
+      if (_navigator.getCurrentRoutes().length === 1  ) {
+         return false;
+      }
+      _navigator.pop();
+      return true;
+    });
+  }
+
   render () {
     return (
       <Navigator
@@ -26,49 +39,97 @@ export default class NotesApp extends Component {
     );
   }
 
-  navigatorRenderScene (route, navigator){
+  navigatorRenderScene = (route, navigator) => {
     _navigator = navigator;
     switch (route.id) {
       case 'ListNotes':
-        return (<ListNotes navigator={navigator} />);
+        return (
+          <ListNotes
+            navigator={navigator}
+            items={this.state.items}
+            addNote={this.addNote}
+            addFolder={this.addFolder}
+          />
+        );
       case 'EditNote':
-        return (<EditNote navigator={navigator} note={route.note} />);
+        return (
+          <EditNote
+            navigator={navigator}
+            note={route.note}
+            updateNote={this.updateNote}
+            saveNote={this.saveNote}
+          />
+        );
     }
+  }
+
+  addNote = () => {
+    this.onDataArrived({id: 3, title: 'new note', content: ''});
+  }
+
+  addFolder = () => {
+    this.onDataArrived({id: 4, title: 'new folder', folder: true})
+  }
+
+  saveNote = () => {
+    if (this.dirtyNote) {
+      const note = this.dirtyNote;
+      this.dirtyNote = null;
+      const items = [...this.state.items];
+      for (let i = 0; i < items.length; i += 1) {
+        if (items[i].id === note.id) {
+          items[i] = note;
+          break;
+        }
+      }
+      this.setState({ items });
+    }
+  }
+
+  updateNote = (note) => {
+    this.dirtyNote = note;
+  }
+
+  onDataArrived(newData) {
+    this.setState({
+      items: this.state.items.concat(newData)
+    });
   }
 }
 
 class ListNotes extends Component {
   constructor() {
     super();
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
-    this.state = {
-      // dataSource: ds.cloneWithRows(['row 1', 'row 2']),
-      dataSource: ds.cloneWithRows([
-        { id: 1, title: 'hello' },
-        { id: 2, title: 'world' }
-      ]),
-    };
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1.id !== r2.id});
+  }
+
+  onActionSelected = (idx) => {
+    if (idx === 0) {
+      this.props.addFolder();
+    }
   }
 
   render() {
+    const { items, addNote } = this.props;
+
     return (
       <View style={{flex:1}}>
-        <Icon.ToolbarAndroid
+        <MaterialIcon.ToolbarAndroid
           style={styles.toolbar}
-          title="Home"
+          title={'Notes'}
           titleColor="white"
-          navIconName="md-arrow-back"
-          onIconClicked={this.props.navigator.pop}
           actions={[
-            { title: 'Settings', iconName: 'md-settings', iconSize: 30, show: 'always' },
-            { title: 'Follow me on Twitter', iconName: 'logo-twitter', iconColor: "#4099FF", show: 'ifRoom' },
+            // { title: 'Settings', iconName: 'settings', show: 'always' },
+            { title: 'Create New Folder', iconName: 'create-new-folder', show: 'always' },
+            { title: 'Menu', iconName: 'more-vert', show: 'always' },
           ]}
-          overflowIconName="md-more"
+          onActionSelected={this.onActionSelected}
         />
         <ListView
-          dataSource={this.state.dataSource}
+          dataSource={this.ds.cloneWithRows(items)}
           renderRow={(rowData) => this.renderListViewRow(rowData)} />
-          <ActionButton buttonColor="rgba(231,76,60,1)">
+          <ActionButton buttonColor="rgba(231,76,60,1)" onPress={addNote} >
+            {/*
             <ActionButton.Item buttonColor='#9b59b6' title="New Task" onPress={() => console.log("notes tapped!")}>
               <Icon name="md-create" style={styles.actionButtonIcon} />
             </ActionButton.Item>
@@ -78,19 +139,22 @@ class ListNotes extends Component {
             <ActionButton.Item buttonColor='#1abc9c' title="All Tasks" onPress={() => {}}>
               <Icon name="md-done-all" style={styles.actionButtonIcon} />
             </ActionButton.Item>
+            */}
           </ActionButton>
       </View>
     );
   }
 
   renderListViewRow(row) {
+    const icon = row.folder
+      ? <FontIcon name={'folder'} style={styles.rowCount} />
+      : <FontIcon name={'file-text-o'} style={styles.rowIcon} />;
+
     return(
         <TouchableHighlight underlayColor={'#f3f3f2'}
                             onPress={() => this.selectRow(row)}>
           <View style={styles.rowContainer}>
-              <Text style={styles.rowCount}>
-                  {row.id}
-              </Text>
+              {icon}
               <View style={styles.rowDetailsContainer}>
                   <Text style={styles.rowTitle}>
                       {row.title}
@@ -116,31 +180,50 @@ class ListNotes extends Component {
 class EditNote extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: props.note.title };
+    this.state = { text: props.note.content };
   }
 
   render() {
-    const { note, navigator } = this.props;
+    const { note, navigator, saveNote } = this.props;
 
     return (
       <View style={{flex: 1}}>
-        <ToolbarAndroid style={styles.toolbar}
-          title={note.title}
-          navIcon={{uri: "ic_arrow_back_white_24dp", isStatic: true}}
-          onIconClicked={navigator.pop}
-          titleColor={'#FFFFFF'}/>
+          <MaterialIcon.ToolbarAndroid
+            style={styles.toolbar}
+            title={note.title}
+            titleColor="white"
+            navIconName="keyboard-arrow-left"
+            onIconClicked={this.saveNoteAndBack}
+            actions={[
+              // { title: 'Settings', iconName: 'md-settings', iconSize: 30, show: 'always' },
+              { title: 'Menu', iconName: 'more-vert', show: 'always' },
+            ]}
+            /*overflowIconName="md-more"*/
+          />
           <TextInput
             style={{flex: 1, borderColor: 'gray', borderWidth: 1}}
             multiline={true}
             textAlignVertical='top'
             underlineColorAndroid='transparent'
-            onChangeText={(text) => this.setState({text})}
-            /*onBlur*/
-            /*onEndEditing*/
+            onChangeText={this.updateNote}
+            onBlur={saveNote}
+            onEndEditing={saveNote}
             value={this.state.text}
           />
         </View>
     );
+  }
+
+  updateNote = (text) => {
+    const { note, updateNote } = this.props;
+    updateNote({...note, content: text});
+    this.setState({ text });
+  }
+
+  saveNoteAndBack = () => {
+    const { navigator, saveNote } = this.props;
+    saveNote();
+    navigator.pop();
   }
 }
 
@@ -160,6 +243,13 @@ const styles = StyleSheet.create({
     },
     rowCount: {
         fontSize: 20,
+        textAlign: 'right',
+        color: 'gray',
+        margin: 10,
+        marginLeft: 15,
+    },
+    rowIcon: {
+        fontSize: 15,
         textAlign: 'right',
         color: 'gray',
         margin: 10,
