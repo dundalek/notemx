18d3c408baefa3066240a4a06b4622166072682a
Jakub Dundalek <dundalek@gmail.com>
2017-03-13 23:55:40 +0100

    Disable context menu animation

--- src/util/CustomMenu.js
+++ src/util/CustomMenu.js
@@ -1,8 +1,6 @@
 // Based on https://github.com/instea/react-native-popup-menu/blob/master/src/renderers/ContextMenu.js
 import React from 'react';
-import { Animated, Easing, StyleSheet } from 'react-native';
-const OPEN_ANIM_DURATION = 225
-const CLOSE_ANIM_DURATION = 195;
+import { View, StyleSheet } from 'react-native';
 
 // Padding is 8 by material design
 export const computePosition = ({ windowLayout, triggerLayout, optionsLayout }) => {
@@ -10,46 +8,15 @@ export const computePosition = ({ windowLayout, triggerLayout, optionsLayout }) 
 };
 
 export default class ContextMenu extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      scaleAnim: new Animated.Value(0.1),
-    };
-  }
-
-  componentDidMount() {
-    Animated.timing(this.state.scaleAnim, {
-      duration: OPEN_ANIM_DURATION,
-      toValue: 1,
-      easing: Easing.out(Easing.cubic),
-    }).start();
-  }
-
-  close() {
-    return new Promise(resolve => {
-      Animated.timing(this.state.scaleAnim, {
-        duration: CLOSE_ANIM_DURATION,
-        toValue: 0,
-        easing: Easing.in(Easing.cubic),
-      }).start(resolve);
-    });
-  }
-
   render() {
     const { style, children, layouts, ...other } = this.props;
-    const animation = {
-      transform: [ { scale: this.state.scaleAnim } ],
-      opacity: this.state.scaleAnim,
-    };
     const position = computePosition(layouts);
     return (
-      <Animated.View {...other} style={[styles.options, style, animation, position]}>
+      <View {...other} style={[styles.options, style, position]}>
         {children}
-      </Animated.View>
+      </View>
     );
   }
-
 }
 
 // public exports
